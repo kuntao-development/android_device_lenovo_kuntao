da920c0ee10803d62400cec7b1c860ee6d680f96
Murat Kozan <muratkozan350@gmail.com>
2021-07-08 20:56:58 +0300

    kuntao: Build xtended themes stub

--- device.mk
+++ device.mk
@@ -396,6 +396,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
+# Xtended Theme
+PRODUCT_PACKAGES += \
+    XtendedThemesStub
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
