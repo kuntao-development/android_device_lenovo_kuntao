67c893bd378c00c4f9a7295f09337b5aaa1ff062
Murat Kozan <muratkozan350@gmail.com>
2021-08-10 17:13:04 +0300

    Revert "kuntao: lenovoparts: Improve FPS Info output formatting and performance"

--- lenovoparts/src/com/lenovo/parts/FPSInfoService.java
+++ lenovoparts/src/com/lenovo/parts/FPSInfoService.java
@@ -74,12 +74,15 @@ public class FPSInfoService extends Service {
 
         private Handler mCurFPSHandler = new Handler() {
             public void handleMessage(Message msg) {
-                if(msg.obj == null || msg.what != 1)
+                if(msg.obj==null){
                     return;
-
-                mFps = parseMeasuredFps((String) msg.obj);
-                mDataAvail = true;
-                updateDisplay();
+                }
+                if(msg.what==1){
+                    String msgData = (String) msg.obj;
+                    msgData = msgData.substring(0, Math.min(msgData.length(), 9));
+                    mFps = msgData;
+                    mDataAvail = true;
+                    updateDisplay();
                 }
             }
         };
@@ -106,7 +109,7 @@ public class FPSInfoService extends Service {
             float descent = mOnlinePaint.descent();
             mFH = (int)(descent - mAscent + .5f);
 
-            final String maxWidthStr = "FPS: XYZ";
+            final String maxWidthStr="60.1";
             mMaxWidth = (int)mOnlinePaint.measureText(maxWidthStr);
 
             updateDisplay();
@@ -153,17 +156,6 @@ public class FPSInfoService extends Service {
             canvas.drawText(s, LEFT-mPaddingLeft-mMaxWidth,
                     y-1, mOnlinePaint);
             y += mFH;
-        }
-
-        private String parseMeasuredFps(String data) {
-            String result = "err";
-            try {
-                float fps = Float.parseFloat(data.trim().split("\\s+")[1]);
-                result = String.valueOf(Math.round(fps));
-            } catch (NumberFormatException e) {
-                Log.e(TAG, "NumberFormatException occured at parsing FPS data");
-            }
-            return "FPS: " + result;
         }
 
         void updateDisplay() {
