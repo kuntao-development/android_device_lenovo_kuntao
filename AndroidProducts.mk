125a1a645523d239843fe330053d17c14dcea6de
ShadesTools <91476790+ShadesTools@users.noreply.github.com>
2021-10-30 00:24:36 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aicp_kuntao.mk
+    $(LOCAL_DIR)/lineage_kuntao.mk
 
 COMMON_LUNCH_CHOICES := \
-    aicp_kuntao-userdebug \
+    lineage_kuntao-userdebug \
