515a622f84e729e03f218fae9471339e3ae2bd95
Murat Kozan <muratkozan350@gmail.com>
2022-12-31 20:04:28 +0300

    kuntao: Time to DerpFest 13

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/awaken_kuntao.mk
+    $(LOCAL_DIR)/derp_kuntao.mk
 
 COMMON_LUNCH_CHOICES := \
-    awaken_kuntao-userdebug
+    derp_kuntao-userdebug
