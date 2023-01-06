5826911a715048584bb553e77f430817e66e0fb5
Murat Kozan <muratkozan350@gmail.com>
2023-01-07 01:30:23 +0300

    kuntao: Rename to bliss kuntao

--- bliss_kuntao.mk
+++ bliss_kuntao.mk
@@ -24,21 +24,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
-# Inherit some common AwakenOS stuff.
-$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lenovo/kuntao/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := awaken_kuntao
+PRODUCT_NAME := bliss_kuntao
 PRODUCT_DEVICE := kuntao
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := Lenovo P2a42
 PRODUCT_MANUFACTURER := LENOVO
 
-# Awaken
-USE_PIXEL_CHARGING := true
+# Bliss
+BLISS_BUILD_VARIANT := vanilla
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo
