88e592da948556d33866ef2af0e3b2644b4df30b
Murat Kozan <muratkozan350@gmail.com>
2023-01-07 00:51:32 +0300

    kuntao: Add livedisplay support

--- device.mk
+++ device.mk
@@ -303,6 +303,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.kuntao
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service-sysfs
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
