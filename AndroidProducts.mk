71fbe3628daaa2d33af5f64af3ac0c33ed940f8a
Murat Kozan <muratkozan350@gmail.com>
2023-02-10 19:41:20 +0300

    Time to RiceDroid a13

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/awaken_kuntao.mk
+    $(LOCAL_DIR)/lineage_kuntao.mk
 
 COMMON_LUNCH_CHOICES := \
-    awaken_kuntao-userdebug
+    lineage_kuntao-userdebug
