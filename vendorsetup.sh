b769942539a4ca738b4a36ebf94698d6c5ce263f
Murat Kozan <muratkozan350@gmail.com>
2023-02-10 19:12:38 +0300

    kuntao: Update vendorsetup.sh for RiceDroid

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,59 +10,59 @@ rm -rf vendor/lenovo/kuntao && git clone https://github.com/kuntao-development/p
 
 echo 'Cloning frameworks base [3/18]'
 # Frameworks base
-rm -rf frameworks/base && git clone --depth=1 https://github.com/kuntao-development/android_frameworks_base -b triton frameworks/base
+rm -rf frameworks/base && git clone --depth=1 https://github.com/Astridxx/android_frameworks_base -b thirteen frameworks/base
 
 echo 'Cloning frameworks native [4/18]'
 # Native
-rm -rf frameworks/native && git clone https://github.com/Astridxx/android_frameworks_native -b triton frameworks/native
+rm -rf frameworks/native && git clone https://github.com/Astridxx/android_frameworks_native-1 -b thirteen frameworks/native
 
 echo 'Cloning frameworks libs net [5/18]'
 # Libs net
-rm -rf frameworks/libs/net && git clone https://github.com/kuntao-development/frameworks_libs_net -b lineage-20.0 frameworks/libs/net
+rm -rf frameworks/libs/net && git clone https://github.com/Astridxx/android_frameworks_libs_net -b thirteen frameworks/libs/net
 
-echo 'Cloning connectivity modules [6/18]'
+echo 'Cloning system memory lmkd [6/18]'
+# Lmkd
+rm -rf system/memory/lmkd && git clone https://github.com/LineageOS/android_system_memory_lmkd -b lineage-20.0 system/memory/lmkd
+
+echo 'Cloning connectivity modules [7/18]'
 # Connectivity
-rm -rf packages/modules/Connectivity && git clone https://github.com/kuntao-development/android_packages_modules_Connectivity -b triton packages/modules/Connectivity
+rm -rf packages/modules/Connectivity && git clone https://github.com/Astridxx/android_packages_modules_Connectivity -b thirteen packages/modules/Connectivity
 
-echo 'Cloning display hal [7/18]'
+echo 'Cloning display hal [8/18]'
 # Clang
 rm -rf hardware/qcom-caf/msm8996/display && git clone https://github.com/kuntao-development/android_hardware_qcom_display -b lineage-20.0-caf-msm8996 hardware/qcom-caf/msm8996/display
 
-echo 'Cloning networkstack modules [8/18]'
+echo 'Cloning networkstack modules [9/18]'
 # Networkstack
 rm -rf packages/modules/NetworkStack && git clone https://github.com/kuntao-development/android_packages_modules_NetworkStack -b lineage-20.0 packages/modules/NetworkStack
 
-echo 'Cloning sepolicy legacy um [9/18]'
+echo 'Cloning sepolicy legacy um [10/18]'
 # Sepolicy-legacy-um
-rm -rf device/qcom/sepolicy-legacy-um && git clone https://github.com/kuntao-development/android_device_qcom_sepolicy -b lineage-20.0-legacy-um device/qcom/sepolicy-legacy-um
+rm -rf device/qcom/sepolicy-legacy-um && git clone https://github.com/kuntao-development/android_device_qcom_sepolicy -b rice-20.0-legacy-um device/qcom/sepolicy-legacy-um
 
-echo 'Cloning system netd [10/18]'
+echo 'Cloning system netd [11/18]'
 # Netd
-rm -rf system/netd && git clone https://github.com/kuntao-development/platform_system_netd -b lineage-20.0 system/netd
+rm -rf system/netd && git clone https://github.com/Astridxx/android_system_netd-1 -b thirteen system/netd
 
-echo 'Cloning system bpf [11/18]'
+echo 'Cloning system bpf [12/18]'
 # Bpf
 rm -rf system/bpf && git clone https://github.com/kuntao-development/android_system_bpf -b lineage-20.0 system/bpf
 
-echo 'Cloning system libhidl [12/18]'
+echo 'Cloning system libhidl [13/18]'
 # Hidl
 rm -rf system/libhidl && git clone https://github.com/kuntao-development/android_system_libhidl -b lineage-20.0 system/libhidl
 
-echo 'Cloning system sepolicy [13/18]'
+echo 'Cloning system sepolicy [14/18]'
 # Sepolicy
-rm -rf system/sepolicy && git clone https://github.com/kuntao-development/android_system_sepolicy -b lineage-20.0 system/sepolicy
+rm -rf system/sepolicy && git clone https://github.com/Astridxx/android_system_sepolicy -b lineage-20.0 system/sepolicy
 
-echo 'Cloning system core [14/18]'
+echo 'Cloning system core [15/18]'
 # Core
-rm -rf system/core && git clone https://github.com/kuntao-development/android_system_core -b triton system/core
+rm -rf system/core && git clone https://github.com/Astridxx/android_system_core -b thirteen system/core
 
-echo 'Cloning system logging [15/18]'
-# Logging
-rm -rf system/logging && git clone https://github.com/kuntao-development/android_system_logging -b lineage-20.0 system/logging
-
-echo 'Cloning vendor awaken [16/18]'
-# Awaken vendor
-rm -rf vendor/awaken && git clone https://github.com/kuntao-development/android_vendor_awaken -b triton vendor/awaken
+echo 'Cloning vendor rice [16/18]'
+# Rice vendor
+rm -rf vendor/lineage && git clone https://github.com/Astridxx/android_vendor_rice -b thirteen vendor/lineage
 
 echo 'Cloning revamped fmradio [17/18]'
 # Logging
